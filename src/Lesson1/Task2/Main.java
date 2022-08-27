54a25e0509a1f31610b8a9cf1318793678294f88
Sacred <plotnickovleonid@yandex.ru>
2022-08-27 11:24:13 +0300

    Lesson 1, Task 2 with Task* (trace duplicates) Complete

---
+++ src/Lesson1/Task2/Main.java
@@ -0,0 +1,96 @@
+package Lesson1.Task2;
+
+public class Main {
+    /* Sorting array from starting index to ending index which is of size almost RUN (standard value RUN = 32)*/
+    static final int RUN = 4;
+
+    public static void main(String[] args) {
+
+        int[] array = {5, 6, 3, 2, 5, 1, 4, 9};
+        int n = array.length;
+        System.out.print("\nBefore sorting array elements are - ");
+        printArr(array, n);
+        timSort(array, n);
+        System.out.print("\nAfter sorting array elements are -  ");
+        printArr(array, n);
+
+    }
+
+    static void insertionSort(int[] a, int beg, int end) /* function to sort an array with insertion sort */ {
+        int i, j, temp;
+        for (i = beg + 1; i <= end; i++) {
+            temp = a[i];
+            j = i - 1;
+
+            while (j >= beg && temp <= a[j]) {
+                a[j + 1] = a[j];
+                j = j - 1;
+            }
+            a[j + 1] = temp;
+        }
+    }
+
+    /* Function to merge the sorted runs */
+    static void merge(int[] a, int beg, int mid, int end) {
+        int i, j, k;
+        int n1 = mid - beg + 1;
+        int n2 = end - mid;
+        //temporary arrays
+        int[] LeftArray = new int[n1];
+        int[] RightArray = new int[n2];
+        /* copy data to temp arrays */
+        for (i = 0; i < n1; i++)
+            LeftArray[i] = a[beg + i];
+        for (j = 0; j < n2; j++)
+            RightArray[j] = a[mid + 1 + j];
+        i = 0;
+        j = 0;
+        k = beg;
+        while (i < n1 && j < n2) {
+            if (LeftArray[i] <= RightArray[j]) {
+                a[k] = LeftArray[i];
+                i++;
+            } else {
+                a[k] = RightArray[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            a[k] = LeftArray[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            a[k] = RightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    /* function to implement tim sort */
+    static void timSort(int[] a, int n) {
+        /* Sort individual subarrays of size RUN */
+        for (int i = 0; i < n; i += RUN)
+            insertionSort(a, i, Math.min((i + RUN - 1), (n - 1)));
+        // Start merging from size RUN (or 32).
+        for (int size = RUN; size < n; size = 2 * size) {
+            for (int beg = 0; beg < n; beg += 2 * size) {
+                /* find ending point of left subarray. The starting point of right sub array is mid + 1 */
+                int mid = beg + size - 1;
+                int end = Math.min((beg + 2 * size - 1), (n - 1));
+
+                /* Merge subarray a[beg...mid] and a[mid +1...end] */
+                if (mid < end)
+                    merge(a, beg, mid, end);
+            }
+        }
+    }
+
+    /* function to print the array elements */
+    static void printArr(int[] a, int n) {
+        for (int i = 0; i < n; i++)
+            System.out.print(a[i] + " ");
+    }
+
+}
